cbbfcb170d394fbc8b41ef4e1a493686ec5a46a4
vignesh <vignesh.srgm@gmail.com>
2021-07-02 22:11:59 +0530

    Intial commit for file

---
+++ src/main/java/com/procodedevs/LearnMain.java
@@ -0,0 +1,13 @@
+package com.procodedevs;
+
+/**
+ * Learn Main
+ * @author Vignesh Rajasekar
+ *
+ */
+public class LearnMain {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+	}
+}
