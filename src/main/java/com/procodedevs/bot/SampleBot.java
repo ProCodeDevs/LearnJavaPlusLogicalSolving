91e20f9cad72ac6f52f2cd164be0aa6a510c3130
vignesh <vignesh.srgm@gmail.com>
2021-07-12 21:41:23 +0530

    let cehck again

--- src/main/java/com/procodedevs/bot/SampleBot.java
+++ src/main/java/com/procodedevs/bot/SampleBot.java
@@ -1,6 +1,7 @@
 package com.procodedevs.bot;
 
 import java.awt.Robot;
+import java.awt.event.KeyEvent;
 
 public class SampleBot {
 
@@ -8,8 +9,8 @@ public class SampleBot {
 	public static void myBot(String command) throws Exception{
 		Robot robot = new Robot();   
 		for(int i=0;i<command.length();i++) {
-			int code = command.charAt(i);
-			robot.keyPress(code);
+			int keyCode = KeyEvent.getExtendedKeyCodeForChar(command.charAt(i));
+			robot.keyPress(keyCode);
 			Thread.sleep(500);
 		}
 	}   
